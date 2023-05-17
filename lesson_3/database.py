ea5952eadcd8aaa1f550b567e4ce41f5ace3345e
rellikevol <rellikevols@gmail.com>
2023-05-17 14:45:44 +0600

    lesson_3_89

---
+++ lesson_3/database.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+
+def messages():
+    conn = sqlite3.connect('artikles.db')
+    cursr = conn.cursor()
+    cursr.execute('SELECT * FROM messages')
+    res = cursr.fetchall()
+    cursr.close()
+    conn.close()
+    return res
+
+
+def articles():
+    conn = sqlite3.connect('artikles.db')
+    cursr = conn.cursor()
+    cursr.execute('SELECT * FROM articles')
+    res = cursr.fetchall()
+    cursr.close()
+    conn.close()
+    return res
